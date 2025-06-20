aac6242dcf04210dc72fbf62994b6524286dd341
Prana dhikasara <pranadhika22@gmail.com>
2025-06-20 15:57:12 +0700

    feat: set up multi module for feature news, core-ui, core-network

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "News App"
+include(":app")
+include(":feature-news")
+include(":core:core-ui")
+include(":core:core-network")
